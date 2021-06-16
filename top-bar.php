ca4ce574cfa7b656b42c88e68d406543007c1808
reitrumpet <rm091291@gmail.com>
2021-06-15 22:57:00 -0300

    Correção na barra do topo

--- top-bar.php
+++ top-bar.php
@@ -1,6 +1,33 @@
 <?php $url = add_query_arg(array(), $wp->request); ?>
 <div class="container-fluid">
-	<div class="row">
+<!-- <div class="row">
+	<nav class="navbar navbar-expand-lg navbar-light bgNav z2 ">
+		<div class="logo col-4">
+			<?php the_custom_logo(); ?>
+		</div>
+		<button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse negrito z1 d-flex justify-content-end col-8" id="navbarTogglerDemo01">
+			<ul class="navbar-nav mr-auto">
+						
+				<li class="nav-item text-center <?php if($url=='planos'){echo 'active';} ?>">
+					<a class="fontAzulVibe <?php if($url=='planos'){echo 'active';} ?>" href="planos" title="Planos">Planos</a>
+				</li>
+				<li class="nav-item text-center <?php if($url=='blog'){echo 'active';} ?>">
+					<a class="fontAzulVibe <?php if($url=='blog'){echo 'active';} ?>" href="blog" title="Blog">Blog </a>
+				</li>
+				<li class="nav-item text-center <?php if($url=='contato'){echo 'active';} ?>">
+					<a class="fontAzulVibe <?php if($url=='contato'){echo 'active';} ?>" href="contato" title="Contato">Contato </a>
+				</li>
+				<li class="nav-item text-center actived">
+					<a class="fontAzulVibe active" href="http://vibetv.com.br/central" title="Área do Cliente" target="_blank">Minha Vibe</a>
+				</li>
+			</ul> 
+		</div>
+	</nav>
+</div> -->
+<div class="row">
 		<nav class="navbar navbar-expand-lg navbar-light col-12 bgNav z2 .wrappe">
 			<div class="col-2 logo">
 				<?php the_custom_logo(); ?>
@@ -11,14 +38,7 @@
 			</button>
 
 			<div class="collapse navbar-collapse negrito col-10 z1" id="navbarTogglerDemo01">
-				<ul class="navbar-nav mr-auto col-12">
-					
-					<li class="nav-item col-12 col-md-2 text-center <?php if($url=='sobre-nos'){echo 'active';} ?>">
-						<a class="fontAzulVibe col-12 <?php if($url=='sobre-nos'){echo 'active';} ?>" href="sobre-nos" title="Sobre Nós">Sobre Nós</a>
-					</li>
-					<li class="nav-item col-12 col-md-2 text-center <?php if($url=='vibe-social'){echo 'active';} ?>">
-						<a class="fontAzulVibe col-12 <?php if($url=='vibe-social'){echo 'active';} ?>" href="vibe-social" title="Vibe Social">Vibe Social </a>
-					</li>
+				<ul class="navbar-nav mr-auto col-12 d-flex justify-content-end">
 					<li class="nav-item col-12 col-md-2 text-center <?php if($url=='planos'){echo 'active';} ?>">
 						<a class="fontAzulVibe col-12 <?php if($url=='planos'){echo 'active';} ?>" href="planos" title="Planos">Planos</a>
 					</li>
@@ -32,12 +52,6 @@
 						<a class="fontAzulVibe col-12 active" href="http://vibetv.com.br/central" title="Área do Cliente" target="_blank">Minha Vibe</a>
 					</li>
 				</ul> 
-
-				<!-- <?php wp_nav_menu(
-					array(
-						'theme_location'=>'top_menu'
-					)
-					); ?> -->
 
 				</div>
 			</nav>
